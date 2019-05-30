ef7fd292eabbdf28e6494e096910c4c0387352db
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-05-30 23:38:36 +0100

    setup loading spinner

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,8 +1,32 @@
 <template>
   <v-container text-xs-center>
+
+    <v-layout row>
+      <v-dialog
+        v-model="loading"
+        persistent
+        fullscreen
+      >
+        <v-container fill-height>
+          <v-layout
+            row
+            justify-center
+            align-center
+          >
+            <v-progress-circular
+              indeterminate
+              :size="70"
+              width="7"
+              color="
+              secondary"
+            ></v-progress-circular>
+          </v-layout>
+        </v-container>
+      </v-dialog>
+    </v-layout>
     <v-flex xs12>
       <v-carousel
-        v-if="posts.length > 0"
+        v-if="!loading && posts.length > 0"
         v-bind="{'cycle': true}"
         interval="3000"
       >
@@ -20,16 +44,14 @@
 
 <script>
 import { gql } from "apollo-boost";
-
+import { mapGetters } from "vuex";
 export default {
   name: "home",
   created() {
     this.handleGetCarouselPosts();
   },
   computed: {
-    posts() {
-      return this.$store.getters.posts;
-    }
+    ...mapGetters(["loading", "posts"])
   },
   methods: {
     handleGetCarouselPosts() {
--- client/src/store.js
+++ client/src/store.js
@@ -8,14 +8,19 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     posts: [],
+    loading: false,
   },
   mutations: {
     setPosts: (state, payload) => {
       state.posts = payload;
     },
+    setLoading: (state, payload) => {
+      state.loading = payload;
+    },
   },
   actions: {
     getPosts: ({ commit }) => {
+      commit('setLoading', true);
       apolloClient
         .query({
           query: gql`
@@ -32,14 +37,17 @@ export default new Vuex.Store({
         })
         .then(({ data }) => {
           commit('setPosts', data.getPosts);
+          commit('setLoading', false);
           console.log(data.getPosts);
         })
         .catch((err) => {
           console.log(err);
+          commit('setLoading', false);
         });
     },
   },
   getters: {
     posts: state => state.posts,
+    loading: state => state.loading,
   },
 });
