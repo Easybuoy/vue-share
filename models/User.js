ebb81624e033f87e1dbbdd6c820446509954298e
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-05-31 20:10:41 +0100

    add mutations, queries and resolvers

--- models/User.js
+++ models/User.js
@@ -1,34 +1,59 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import md5 from 'md5';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     unique: true,
-    trim: true
+    trim: true,
   },
   email: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   password: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   avatar: {
-    type: String
+    type: String,
   },
   joinDate: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   favourites: {
     type: [mongoose.Schema.Types.ObjectId],
     required: true,
-    ref: "Post"
-  }
+    ref: 'Post',
+  },
 });
 
-export default mongoose.model("User", UserSchema);
+// Create avatar for user
+UserSchema.pre('save', function (next) {
+  this.avatar = `http://gravatar.com/avatar/${md5(this.username)}?d=identicon`;
+  next();
+});
+
+// Hash password
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err2, hash) => {
+      if (err2) return err2;
+      this.password = hash;
+      next();
+    });
+  });
+});
+
+export default mongoose.model('User', UserSchema);
